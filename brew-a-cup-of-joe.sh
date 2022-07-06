1f57f6f17e4ddd3712caab162aec0bc84c4ed5f4
jwoods013 <joewoods@traziventures.com>
2022-07-06 15:34:43 -0700

    front-end vs backend logic

--- brew-a-cup-of-joe.sh
+++ brew-a-cup-of-joe.sh
@@ -1,3 +1,6 @@
+read -p 'Do you want Front-end package(y/n)? ' frontend
+read -p 'Do you want Back-end package(y/n)? ' backend
+
 echo "Installing brew..."
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
@@ -6,8 +9,8 @@ brew install nvm
 
 echo "Writing nvm dir to ~/.zshrc"
 echo “export NVM_DIR="$HOME/.nvm"
-  [ -s "/usr/local/opt/nvm/nvm.sh" ] && \. "/usr/local/opt/nvm/nvm.sh"  # This loads nvm
-  [ -s "/usr/local/opt/nvm/etc/bash_completion.d/nvm" ] && \. "/usr/local/opt/nvm/etc/bash_completion.d/nvm" >> ~/.zshrc
+[ -s "/usr/local/opt/nvm/nvm.sh" ] && \. "/usr/local/opt/nvm/nvm.sh"  # This loads nvm
+[ -s "/usr/local/opt/nvm/etc/bash_completion.d/nvm" ] && \. "/usr/local/opt/nvm/etc/bash_completion.d/nvm" >> ~/.zshrc
 
 echo "Installing node 16.15.1..."
 nvm install 16.15.1
@@ -16,31 +19,46 @@ echo "Instlling Brave Browser..."
 brew install --cask brave-browser
 
 echo "Installing discord..."
-brew install --cask discord 
-
-echo "Installing Visual Studio Code..."
-brew install --cask visual-studio-code
-
-echo "Installing Github Desktop..."
-brew install --cask github
+brew install --cask discord
 
 echo "Installing Postman..."
 brew install --cask postman
 
-echo "Installing PgAdmin4..."
-brew install --cask pgadmin4
+echo "Installing Github Desktop..."
+brew install --cask github
 
 echo "Installing Flameshot screen..."
 brew install --cask flameshot
 
-echo "Installing PyEnv..."
-brew install pyenv
+if [ $backend = "y" ] || then
 
-echo "Installing Python 3.10.5..."
-pyenv install 3.10.5
+    echo "Installing Visual Studio Code..."
+    brew install --cask visual-studio-code
 
-echo "Installing nuke-it...."
-npm install —location=global nuke-it 
+    echo "Installing PgAdmin4..."
+    brew install --cask pgadmin4
 
-echo "Install postgresql..."
-brew install postgresql
+    echo "Installing PyEnv..."
+    brew install pyenv
+
+    echo "Installing Python 3.10.5..."
+    pyenv install 3.10.5
+
+    echo "Installing Nuke It...."
+    npm install —location=global nuke-it 
+
+    echo "Install Postgresql..."
+    brew install postgresql
+
+    echo "Installing Redis..."
+    brew install redis
+fi
+
+if [ $frontend = "y" ] then
+
+    echo "Installing PHPStorm..."
+    brew install --cask phpstorm
+
+    echo "Installing Fork..."
+    brew install --cask fork
+fi
